86a5606ab1f9cdf694d1c1963ff6aac1ee50a0b5
Graham Steele <graham.steele@me.com>
2016-07-10 21:52:52 +0100

    Message was changed

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! I've changed the message!");
 
 	}
 
